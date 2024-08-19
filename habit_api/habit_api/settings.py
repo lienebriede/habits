91179005b525da1237d0565514decd264a934c52
Liene Briede <lienebriede84@gmail.com>
2024-08-19 17:49:50 +0000

    Add authentication handling

--- habit_api/habit_api/settings.py
+++ habit_api/habit_api/settings.py
@@ -57,6 +57,16 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
 ROOT_URLCONF = 'habit_api.urls'
 
 TEMPLATES = [
