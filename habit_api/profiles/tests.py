dfc44e19e903c5a4f3a6c647957005f7d39746c0
Liene Briede <lienebriede84@gmail.com>
2024-08-19 17:27:38 +0000

    Test API to correctly return a 404 Profile Not Found

--- habit_api/profiles/tests.py
+++ habit_api/profiles/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
+from profiles.models import Profile
 
-# Create your tests here.
+class ProfileDetailTests(APITestCase):
+
+    def setUp(self):
+        # Create ans authenticate user
+        self.user = User.objects.create_user(username='tuser', password='tpass')
+        self.client.login(username='tuser', password='tpass')
+
+    def test_get_profile_not_found(self):
+        """
+        Ensure that a 404 status code is returned when the profile does not exist.
+        """
+        Profile.objects.filter(owner=self.user).delete()
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data['detail'], 'Profile not found')
