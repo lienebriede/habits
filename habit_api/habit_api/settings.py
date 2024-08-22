722d74b683fe4280528467cb1a3d86d99bd4bf1c
Liene Briede <lienebriede84@gmail.com>
2024-08-22 12:30:43 +0000

    Add tokens

--- habit_api/habit_api/settings.py
+++ habit_api/habit_api/settings.py
@@ -22,7 +22,6 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -53,10 +52,26 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
+    'rest_framework.authtoken', 
+    'dj_rest_auth',
     'profiles',
     'habits',
     'couplings',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [( 
+        'rest_framework.authentication.SessionAuthentication' 
+        if 'DEV' in os.environ 
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )]
+    }
+REST_USE_JWT = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_SECURE = True
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'}
+
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-lienebriede-habits-j4pln3ur8hf.ws.codeinstitute-ide.net',
